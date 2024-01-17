2ee3f93cc9e90fd1bc21f6a06913a4e05a5046db
emgardner <gardner.ethan10@gmail.com>
2024-01-17 07:36:37 -0500

    Don't use this or you will get smacked with a rate limit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use reqwest;
 use reqwest::{ Result as ReqwestResult };
-use scraper::{Html, Selector, Element, ElementRef};
+use scraper::{Html, Selector, ElementRef};
+use chrono::{Datelike, NaiveDate, Duration, Local};
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use serde::{Serialize, Deserialize};
+
+fn generate_all_days(year: i32, month: u32, day: u32) -> Vec<String> {
+    let mut dates = Vec::new();
+    let start_date = NaiveDate::from_ymd(year, month, day);
+    let end_date = Local::now().naive_local().date();
+    let mut current_date = start_date;
+    while current_date < end_date {
+        dates.push(current_date.format("%Y-%m-%d").to_string());
+        current_date += Duration::days(1);
+    }
+    dates
+}
+
+
 
 #[derive(Debug)]
 pub enum RowType {
@@ -26,11 +44,11 @@ pub struct Info {
     comments: String
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Post {
     id: String,
-    // rank: String,
-    rank: i32,
+    rank: String,
+    // rank: i32,
     titleline: String,
     link: String,
     // score: i32, 
@@ -41,87 +59,156 @@ pub struct Post {
     comments: String
 }
 
-async fn get_page() -> ReqwestResult<String> {
-    reqwest::get("https://news.ycombinator.com/news")
+async fn get_page(params: &PageParams) -> ReqwestResult<String> {
+    // reqwest::get("https://news.ycombinator.com/news")
+    reqwest::get(format_request(params))
         .await?
         .text()
-        .await
+    .await
 }
 
+#[derive(Debug)]
+pub struct PageParams {
+    day: String,
+    page: u32
+}
+
+fn format_request(params: &PageParams) -> String {
+    format!("https://news.ycombinator.com/front?day={}&p={}", params.day, params.page)
+}
+
+fn parse_thing(row: &ElementRef<'_>) -> Option<Thing> {
+    let link_selector = Selector::parse("td > span > a").unwrap();
+    let rank_selector = Selector::parse("span.rank").unwrap();
+    let id = row.value().attr("id")?;
+    let link_ele = row.select(&link_selector).next()?;
+    let rank_ele = row.select(&rank_selector).next()?;
+    let link =  link_ele.value().attr("href")?;
+    Some(Thing {
+        id: id.to_string(),
+        rank: rank_ele.inner_html(),
+        titleline: link_ele.inner_html(),
+        link: link.to_string()
+    })
+}
+
+fn parse_row(subline: &ElementRef<'_>) -> Option<Info> {
+    let score_selector = Selector::parse("span.score").unwrap();
+    let user_selector = Selector::parse(".hnuser").unwrap();
+    let date_selector = Selector::parse("span.age").unwrap();
+    let comment_selector = Selector::parse("a").unwrap();
+    let score = subline.select(&score_selector).next()?;
+    let user = subline.select(&user_selector).next()?;
+    let date = subline.select(&date_selector).next()?.value().attr("title")?;
+    let comment = subline.select(&comment_selector)
+        .filter(|e| e.inner_html().contains("comments"))
+        .nth(0)?;
+    return Some( Info {
+        score: score.inner_html().to_string(),
+        user: user.inner_html().to_string(),
+        date: date.to_string(), 
+        comments: comment.inner_html()
+    });
+}
 
 fn process_row(row: &ElementRef) -> Option<RowType> {
     let class = row.value().attr("class").unwrap_or("") ;
-    let link_selector = Selector::parse("td > span > a").unwrap();
-    let rank_selector = Selector::parse("span.rank").unwrap();
     let subline_selector = Selector::parse("span.subline").unwrap();
     match class {
         "spacer" => return Some(RowType::Spacer),
         "athing" => {
-            let id = row.value().attr("id").unwrap();
-            let link_ele = row.select(&link_selector).next().unwrap();
-            let rank_ele = row.select(&rank_selector).next().unwrap();
-            return Some(RowType::Thing(Thing {
-                id: id.to_string(),
-                rank: rank_ele.inner_html(),
-                titleline: link_ele.inner_html(),
-                link: link_ele.value().attr("href").unwrap().to_string()
-            }))
+            match parse_thing(row) {
+                Some(thing) => Some(RowType::Thing(thing)),
+                None => None
+            }
         },
         _ => {
             let subline = row.select(&subline_selector).next();
             match subline {
                 Some(s) => {
-                    let score_selector = Selector::parse("span.score").unwrap();
-                    let user_selector = Selector::parse(".hnuser").unwrap();
-                    let date_selector = Selector::parse("span.age").unwrap();
-                    let comment_selector = Selector::parse("a").unwrap();
-
-                    // #hnmain > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(5) > td.subtext > span > a:nth-child(6)
-                    let score = s.select(&score_selector).next();
-                    let user = s.select(&user_selector).next();
-                    let date = s.select(&date_selector).next();
-                    let comment = s.select(&comment_selector)
-                        .filter(|e| e.inner_html().contains("comments"))
-                        // .collect::<Vec<ElementRef>>()
-                        .nth(0)
-                        .map_or("".to_string(), |e| e.inner_html().to_string());
-                    return Some(RowType::Info( Info {
-                        score: score.map_or("".to_string(), |e| e.inner_html().to_string()),
-                        user: user.map_or("".to_string(), |e| e.inner_html().to_string()),
-                        date: date.map_or("".to_string(), |e| e.value().attr("title").unwrap_or("").to_string()),
-                        // comments: comment.map_or("".to_string(), |e| e.inner_html().to_string())
-                        comments: comment
-                    }));
+                    match parse_row(&s) {
+                        Some(info) => Some(RowType::Info(info)),
+                        None => None
+                    }
                 },
-                _ => return None
+                _ => None
             }
         }
-    };
-    
-    None
+    }
+
 }
 
 
-fn get_posts(document: &str) {
+fn get_posts(document: &str) -> Option<Vec<Post>> {
     let document = Html::parse_document(document);
     let main_table_selector = Selector::parse("#hnmain > tbody > tr:nth-child(3) > td > table").unwrap();
     let tr_selector = Selector::parse("tr").unwrap();
-    let main_table = document.select(&main_table_selector).next().unwrap();
-    let table_rows = main_table.select(&tr_selector);
-    for row in table_rows {
-        let row_type = process_row(&row);
-        // match row_type {
-        // }
-        // println!("ROW");
-        println!("{:?}", row_type);
+    let main_table = document.select(&main_table_selector).next()?;
+    let mut posts: Vec<Post> = vec![];
+    let mut table_rows = main_table.select(&tr_selector);
+    while let Some(row) = table_rows.next() {
+        if let Some(row_type) = process_row(&row) {
+            match row_type {
+                RowType::Thing(thing) => {
+                    if let Some(nextrow) = table_rows.next() {
+                        if let Some(inforow) = process_row(&nextrow) {
+                            match inforow {
+                                RowType::Info(info) => {
+                                    posts.push(Post {
+                                        id: thing.id,
+                                        rank: thing.rank,
+                                        titleline: thing.titleline,
+                                        link: thing.link,
+                                        score: info.score, 
+                                        user: info.user,
+                                        date: info.date,
+                                        comments: info.comments
+                                    });
+
+                                },
+                                _ => ()
+                            }
+                        }
+                    }
+                }
+                _ => ()
+            }
+        }
     }
+    Some(posts)
+}
+
+
+async fn get_day_posts(date: String) -> std::io::Result<usize> {
+    let mut params = PageParams {
+        day: date.clone(),
+        page: 0
+    };
+    let mut posts: Vec<Post> = vec![];
+    while let Ok(page) = get_page(&params).await {
+        params.page += 1;
+        if let Some(newposts) = get_posts(&page) {
+            posts.extend(newposts);
+        } else {
+            break;
+        }
+    }
+    let file = File::create(format!("./data/{}.json", date))?;
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer(&mut writer, &posts)?;
+    writer.flush()?;
+    Ok(posts.len())
 }
 
 #[tokio::main]
 async fn main() {
-    let res = get_page().await;
-    match res {
-        Ok(r) => get_posts(&r),
-        Err(ref e) => eprintln!("{e:?}")
+    let days = generate_all_days(2007, 10, 1);
+    // println!("{days:?}");
+    // let res = get_day_posts("2024-01-15".to_string()).await;
+    // println!("{res:?}");
+    for day in days {
+        let res = get_day_posts(day.to_string()).await;
+        println!("{day:?} {res:?}");
     }
+
 }
